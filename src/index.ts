9f374686ce47316cdcb40d5fcae5d318755daf02
Andrés Bedoya <andres@bedoya.co>
2025-06-19 08:39:48 -0500

    Exposed getAvailableTiles

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { ABLeafletMap } from '@/ab-leaflet-map';
 export { registerMarkerType, getAvailableMarkerTypes } from '@/utils/ab-marker-utils';
-export { registerTile } from '@/utils/ab-tile-utils';
+export { registerTile, getAvailableTiles } from '@/utils/ab-tile-utils';
